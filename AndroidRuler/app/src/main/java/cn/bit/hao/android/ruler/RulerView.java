9e50e1ba892448e37bf92c43f683fb39723d5ce8
haogev6 <wuh@hadlinks.com>
2016-05-23 17:19:30 +0800

    fix saveInstance bug

--- AndroidRuler/app/src/main/java/cn/bit/hao/android/ruler/RulerView.java
+++ AndroidRuler/app/src/main/java/cn/bit/hao/android/ruler/RulerView.java
@@ -1,10 +1,13 @@
 package cn.bit.hao.android.ruler;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
@@ -17,163 +20,225 @@ import cn.bit.hao.android.R;
  * @author wuhao at 2016/5/20
  */
 public class RulerView extends View {
-    private static final String TAG = RulerView.class.getSimpleName();
+	private static final String TAG = RulerView.class.getSimpleName();
 
-    private static final String CENTIMETER_UNIT_LABEL = "cm";
-    private static final String INCH_UNIT_LABEL = "in";
+	private static final String CENTIMETER_UNIT_LABEL = "cm";
+	private static final String INCH_UNIT_LABEL = "in";
 
-    private static final float DEFAULT_LONG_SCALE_LENGTH_MM = 5;
-    private static final float DEFAULT_MEDIUM_SCALE_LENGTH_MM = 3.5f;
-    private static final float DEFAULT_SHORT_SCALE_LENGTH_MM = 2.5f;
+	private static final float DEFAULT_LONG_SCALE_LENGTH_MM = 5;
+	private static final float DEFAULT_MEDIUM_SCALE_LENGTH_MM = 3.5f;
+	private static final float DEFAULT_SHORT_SCALE_LENGTH_MM = 2.5f;
+	private static final int DEFAULT_TEXT_SIZE_MM = 3;
+	private static final String ORIENTATION = "ORIENTATION";
+	private float longScaleLength = DEFAULT_LONG_SCALE_LENGTH_MM;
+	private float mediumScaleLength = DEFAULT_MEDIUM_SCALE_LENGTH_MM;
+	private float shortScaleLength = DEFAULT_SHORT_SCALE_LENGTH_MM;
+	private float mTextSize = DEFAULT_TEXT_SIZE_MM;
+	private float mUnitSize = DEFAULT_TEXT_SIZE_MM;
+	private int mTextColor = Color.BLACK;
+	private boolean mShowCentimeter = true;
+	private boolean mShowInch = true;
+	private boolean mOrientationVertical = true;
+	private float oneMillimeter = 1;
+	private float oneInch = 1;
+	private TextPaint mTextPaint;
+	private TextPaint mUnitPaint;
+	private Paint mLinePaint;
+	private float mTextHeight;
+	private float zeroShift = 0;
 
-    private float longScaleLength = DEFAULT_LONG_SCALE_LENGTH_MM;
-    private float mediumScaleLength = DEFAULT_MEDIUM_SCALE_LENGTH_MM;
-    private float shortScaleLength = DEFAULT_SHORT_SCALE_LENGTH_MM;
+	public RulerView(Context context) {
+		super(context);
+		init(null, 0);
+	}
 
-    private static final int DEFAULT_TEXT_SIZE_MM = 3;
-    private float mTextSize = DEFAULT_TEXT_SIZE_MM;
-    private float mUnitSize = DEFAULT_TEXT_SIZE_MM;
+	public RulerView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(attrs, 0);
+	}
 
-    private int mTextColor = Color.BLACK;
+	public RulerView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(attrs, defStyle);
+	}
 
-    private boolean mShowCentimeter = true;
-    private boolean mShowInch = true;
-    private boolean mOrientationVertical = true;
+	public boolean isOrientationVertical() {
+		return mOrientationVertical;
+	}
 
-    private float oneMillimeter = 1;
-    private float oneInch = 1;
+	public void setOrientationVertical(boolean vertical) {
+		mOrientationVertical = vertical;
+		initUnitLength();
+		invalidate();
+	}
 
-    private TextPaint mTextPaint;
-    private TextPaint mUnitPaint;
-    private Paint mLinePaint;
+	@Override
+	protected void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+	}
 
-    private float mTextHeight;
+	@Override
+	protected void onRestoreInstanceState(Parcelable state) {
+		if (!(state instanceof SavedState)) {
+			super.onRestoreInstanceState(state);
+			return;
+		}
+		SavedState savedState = (SavedState) state;
+		super.onRestoreInstanceState(savedState.getSuperState());
 
-    private float zeroShift = 0;
+		mOrientationVertical = savedState.orientationVertical;
+		invalidate();
+	}
 
-    public RulerView(Context context) {
-        super(context);
-        init(null, 0);
-    }
+	@Override
+	protected Parcelable onSaveInstanceState() {
+		SavedState savedState = new SavedState(super.onSaveInstanceState());
+		savedState.orientationVertical = mOrientationVertical;
+		return savedState;
+	}
 
-    public RulerView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(attrs, 0);
-    }
+	private void init(AttributeSet attrs, int defStyle) {
+		// Load attributes
+		final TypedArray a = getContext().obtainStyledAttributes(
+				attrs, R.styleable.RulerView, defStyle, 0);
 
-    public RulerView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init(attrs, defStyle);
-    }
+		mShowCentimeter = a.getBoolean(R.styleable.RulerView_centimeter, mShowCentimeter);
+		mShowInch = a.getBoolean(R.styleable.RulerView_inch, mShowInch);
+		mOrientationVertical = a.getBoolean(R.styleable.RulerView_vertical, mOrientationVertical);
 
-    public boolean isOrientationVertical() {
-        return mOrientationVertical;
-    }
+		a.recycle();
 
-    public void setOrientationVertical(boolean vertical) {
-        mOrientationVertical = vertical;
-        invalidate();
-    }
+		// Set up a default TextPaint object
+		mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+		mTextPaint.setTextAlign(Paint.Align.CENTER);
+		mTextPaint.setColor(mTextColor);
 
-    private void init(AttributeSet attrs, int defStyle) {
-        final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+		mUnitPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+		mUnitPaint.setTextAlign(Paint.Align.LEFT);
+		mUnitPaint.setColor(mTextColor);
 
-        oneMillimeter = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_MM, 1, displayMetrics);
-        oneInch = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_IN, 1, displayMetrics);
+		final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+		mLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		mLinePaint.setStrokeWidth(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, displayMetrics));
+		mLinePaint.setColor(mTextColor);
+		mLinePaint.setStyle(Paint.Style.STROKE);
 
-        longScaleLength = oneMillimeter * DEFAULT_LONG_SCALE_LENGTH_MM;
-        mediumScaleLength = oneMillimeter * DEFAULT_MEDIUM_SCALE_LENGTH_MM;
-        shortScaleLength = oneMillimeter * DEFAULT_SHORT_SCALE_LENGTH_MM;
+		initUnitLength();
+	}
 
-        // Load attributes
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.RulerView, defStyle, 0);
+	private void initUnitLength() {
+		final Configuration configuration = getResources().getConfiguration();
+		final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+		if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+			oneInch = mOrientationVertical ? displayMetrics.ydpi
+					: TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_IN, 1, displayMetrics);
+		} else if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+			oneInch = mOrientationVertical ? TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_IN, 1, displayMetrics)
+					: displayMetrics.ydpi;
+		}
+		oneMillimeter = oneInch * (1.0f / 25.4f);
 
-        mShowCentimeter = a.getBoolean(R.styleable.RulerView_centimeter, mShowCentimeter);
-        mShowInch = a.getBoolean(R.styleable.RulerView_inch, mShowInch);
-        mOrientationVertical = a.getBoolean(R.styleable.RulerView_vertical, mOrientationVertical);
+		longScaleLength = oneMillimeter * DEFAULT_LONG_SCALE_LENGTH_MM;
+		mediumScaleLength = oneMillimeter * DEFAULT_MEDIUM_SCALE_LENGTH_MM;
+		shortScaleLength = oneMillimeter * DEFAULT_SHORT_SCALE_LENGTH_MM;
 
-        mTextSize = oneMillimeter * DEFAULT_TEXT_SIZE_MM;
-        mUnitSize = mTextSize / 1.25f;
+		mTextSize = oneMillimeter * DEFAULT_TEXT_SIZE_MM;
+		mUnitSize = mTextSize / 1.25f;
 
-        a.recycle();
+		// Update TextPaint and text measurements from attributes
+		invalidateTextPaintAndMeasurements();
+	}
 
-        // Set up a default TextPaint object
-        mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.CENTER);
-        mTextPaint.setColor(mTextColor);
+	private void invalidateTextPaintAndMeasurements() {
+		mTextPaint.setTextSize(mTextSize);
+		mUnitPaint.setTextSize(mUnitSize);
 
-        mUnitPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-        mUnitPaint.setTextAlign(Paint.Align.LEFT);
-        mUnitPaint.setColor(mTextColor);
+		final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+		zeroShift = mTextPaint.measureText("0");
 
-        mLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mLinePaint.setStrokeWidth(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, displayMetrics));
-        mLinePaint.setColor(mTextColor);
-        mLinePaint.setStyle(Paint.Style.STROKE);
+		mTextHeight = -mTextPaint.getFontMetrics().top;
+	}
 
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
-    }
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
 
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(mTextSize);
-        mUnitPaint.setTextSize(mUnitSize);
+		canvas.drawColor(Color.WHITE);
 
-        final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        zeroShift = mTextPaint.measureText("0");
+		// allocations per draw cycle.
+		int paddingLeft = getPaddingLeft();
+		int paddingTop = getPaddingTop();
+		int paddingRight = getPaddingRight();
+		int paddingBottom = getPaddingBottom();
 
-        mTextHeight = -mTextPaint.getFontMetrics().top;
+		int contentWidth = getWidth() - paddingLeft - paddingRight;
+		int contentHeight = getHeight() - paddingTop - paddingBottom;
 
-    }
+		int contentLength = contentHeight;
+		int saveCount = 0;
+		if (mOrientationVertical) {
+			saveCount = canvas.save();
+			canvas.rotate(90, getWidth() / 2, getWidth() / 2);
+		} else {
+			contentLength = contentWidth;
+		}
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+		int scale = 0;
+		float scaleShiftLength = scale;
+		while (scaleShiftLength < contentLength) {
+			float scaleLength = shortScaleLength;
+			if (scale % 10 == 0) {
+				scaleLength = longScaleLength;
+				canvas.drawText(String.valueOf(scale / 10), paddingLeft + zeroShift + scaleShiftLength, scaleLength + mTextHeight, mTextPaint);
+				if (scale == 0) {
+					canvas.drawText(CENTIMETER_UNIT_LABEL, paddingLeft + zeroShift * 1.6f + scaleShiftLength, scaleLength + mTextHeight, mUnitPaint);
+				}
+			} else if (scale % 5 == 0) {
+				scaleLength = mediumScaleLength;
+			}
 
-        canvas.drawColor(Color.WHITE);
+			canvas.drawLine(paddingLeft + zeroShift + scaleShiftLength, 0, paddingLeft + zeroShift + scaleShiftLength, scaleLength, mLinePaint);
 
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
+			++scale;
+			scaleShiftLength = oneMillimeter * scale;
+		}
 
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
+		if (mOrientationVertical) {
+			canvas.restoreToCount(saveCount);
+		}
+	}
 
-        int contentLength = contentHeight;
-        int saveCount = 0;
-        if (mOrientationVertical) {
-            saveCount = canvas.save();
-            canvas.rotate(90, getWidth() / 2, getWidth() / 2);
-        } else {
-            contentLength = contentWidth;
-        }
+	public static class SavedState extends BaseSavedState {
+		public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
 
-        int scale = 0;
-        float scaleShiftLength = scale;
-        while (scaleShiftLength < contentLength) {
-            float scaleLength = shortScaleLength;
-            if (scale % 10 == 0) {
-                scaleLength = longScaleLength;
-                canvas.drawText(String.valueOf(scale / 10), paddingLeft + zeroShift + scaleShiftLength, scaleLength + mTextHeight, mTextPaint);
-                if (scale == 0) {
-                    canvas.drawText(CENTIMETER_UNIT_LABEL, paddingLeft + zeroShift * 1.6f + scaleShiftLength, scaleLength + mTextHeight, mUnitPaint);
-                }
-            } else if (scale % 5 == 0) {
-                scaleLength = mediumScaleLength;
-            }
+			public SavedState createFromParcel(Parcel source) {
+				return new SavedState(source);
+			}
 
-            canvas.drawLine(paddingLeft + zeroShift + scaleShiftLength, 0, paddingLeft + zeroShift + scaleShiftLength, scaleLength, mLinePaint);
+			public SavedState[] newArray(int size) {
+				return new SavedState[size];
+			}
+		};
+		public boolean orientationVertical;
 
-            ++scale;
-            scaleShiftLength = oneMillimeter * scale;
-        }
+		public SavedState(Parcelable superState) {
+			super(superState);
+		}
 
-        if (mOrientationVertical) {
-            canvas.restoreToCount(saveCount);
-        }
-    }
+		private SavedState(Parcel in) {
+			super(in);
+			orientationVertical = (in.readInt() != 0);
+		}
 
+		@Override
+		public void writeToParcel(Parcel out, int flags) {
+			super.writeToParcel(out, flags);
+			out.writeInt(orientationVertical ? 1 : 0);
+		}
+
+		@Override
+		public String toString() {
+			return "RulerView.SavedState{orientationVertical" + orientationVertical + "}";
+		}
+	}
 }
